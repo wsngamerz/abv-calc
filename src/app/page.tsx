187a535f355c03d39b5df092a3a3a771bb330012
William Neild <wneild5@gmail.com>
2023-06-18 20:48:32 +0100

    feat: implement application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,184 @@
-import Image from 'next/image'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Trash } from "lucide-react";
+import { useEffect, useState } from "react";
+
+type Ingredient = {
+  name: string;
+  amount: number;
+  abv: number;
+};
 
 export default function Home() {
+  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [abv, setAbv] = useState(0);
+  const [valid, setValid] = useState(false);
+
+  const [totalVol, setTotalVol] = useState(0);
+  const [totalAbv, setTotalAbv] = useState(0);
+
+  /**
+   * Only set valid input if name, amount and abv are valid
+   */
+  useEffect(() => {
+    if (
+      name != "" &&
+      amount > 0 &&
+      amount <= 1000 &&
+      abv >= 0 &&
+      abv <= 100 &&
+      ingredients.find((i) => i.name === name) == undefined
+    ) {
+      setValid(true);
+      return;
+    }
+
+    setValid(false);
+  }, [name, setValid, amount, abv, ingredients]);
+
+  /**
+   * Calculate total volume and abv
+   * when ingredients change
+   */
+  useEffect(() => {
+    // calculate total volume and abv
+    const calcVol = ingredients.reduce((sum, i) => sum + i.amount, 0);
+    const calcAlc = ingredients.reduce(
+      (acc, i) => acc + i.amount * (i.abv / 100),
+      0
+    );
+    const calcAbv = Number(((calcAlc / calcVol) * 100).toFixed(2));
+
+    setTotalVol(calcVol);
+    setTotalAbv(calcAbv || 0);
+  }, [ingredients]);
+
+  const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value));
+  };
+
+  const onAbvChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAbv(Number(e.target.value));
+  };
+
+  const addIngredient = () => {
+    // check if ingredient already exists
+    const exists = ingredients.find((i) => i.name === name);
+
+    if (!exists) {
+      setIngredients((prev) => [...prev, { name, amount, abv }]);
+      setName("");
+      setAmount(0);
+      setAbv(0);
+    }
+  };
+
+  const removeIngredient = (name: string) => {
+    setIngredients((prev) => prev.filter((i) => i.name !== name));
+  };
+
+  const resetIngredients = () => {
+    setIngredients([]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex justify-center w-full my-16">
+      <section className="w-full md:max-w-3xl border rounded p-4 m-4 space-y-4 bg-white drop-shadow-sm">
+        <h1 className="font-bold text-xl text-center">
+          Cocktail ABV Calculator
+        </h1>
+
+        <div className="bg-gray-50 border rounded p-4 space-y-2">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+            <div className="grid items-center gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                className="bg-white"
+                type="text"
+                value={name}
+                onChange={onNameChange}
+              />
+            </div>
+
+            <div className="grid items-center gap-2">
+              <Label htmlFor="amount">Amount (ml)</Label>
+              <Input
+                id="amount"
+                className="bg-white"
+                type="number"
+                value={amount}
+                onChange={onAmountChange}
+              />
+            </div>
+
+            <div className="grid items-center gap-2">
+              <Label htmlFor="abv">ABV%</Label>
+              <Input
+                id="abv"
+                className="bg-white"
+                type="number"
+                value={abv}
+                onChange={onAbvChange}
+              />
+            </div>
+          </div>
+          <Button
+            variant="default"
+            className="w-full"
+            disabled={!valid}
+            onClick={addIngredient}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Add Ingredient
+          </Button>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div>
+          <h2 className="font-bold text-lg">Ingredients</h2>
+          {ingredients.length == 0 && <span>No ingredients</span>}
+          <ul className="space-y-2">
+            {ingredients.map((i) => (
+              <li key={i.name}>
+                <div className="space-x-2 flex items-center">
+                  <span className="font-semibold">{i.name}</span>
+                  <span className="text-sm text-gray-500">
+                    ({i.amount}ml{i.abv > 0 && ` @ ${i.abv}%`})
+                  </span>
+                  <Trash
+                    size={16}
+                    className="text-gray-500 hover:text-red-500 cursor-pointer"
+                    onClick={() => removeIngredient(i.name)}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div>
+          <h2 className="font-bold text-lg">Calculated</h2>
+          <ul>
+            <li>Total Vol: {totalVol}ml</li>
+            <li>Total ABV: {totalAbv}%</li>
+          </ul>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="flex justify-end">
+          <Button variant="outline" onClick={resetIngredients}>
+            Reset
+          </Button>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
